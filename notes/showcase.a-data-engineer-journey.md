cc50f68bf016a3dba7f27e3d10da87d304c86e5a
Kaung SiThu <kaungsithu.kaungsithu@gmail.com>
2025-02-12 20:26:12 +0630

    added showcase item - DE journey

---
+++ notes/showcase.a-data-engineer-journey.md
@@ -0,0 +1,7 @@
+---
+id: ohvpqwgil5q4ka74vxkmctc
+title: A Data Engineer Journey
+desc: ''
+updated: 1739368447222
+created: 1739368447223
+---
--- sidebar.js
+++ sidebar.js
@@ -1,66 +1,51 @@
 module.exports = [
-    {
-      type: 'note',
-      id: 'about',
-      label: 'About'
-    },
-    {
-      type: 'category',
-      label: 'Career Journey',
-      link: {type: 'note', id: 'career-journey'},
-      items: [
-        {
-          type: 'note',
-          id: 'experience',
-          label: 'Experience'
-        },
-        {
-          type: 'note',
-          id: 'skills',
-          label: 'Skills'
-        },
-        {
-          type: 'note',
-          id: 'certifications',
-          label: 'Certifications'
-        },
-        {
-          type: 'note',
-          id: 'education',
-          label: 'Education'
-        },
-        {
-          type: 'note',
-          id: 'additional-experience',
-          label: 'Additional Experience'
-        }
-      ]
-    },
-    {
-      type: 'category',
-      label: 'Showcase',
-      link: {type: 'note', id: 'showcase'}
-      // items: [
-      //   {
-      //     type: 'note',
-      //     id: 'experience'
-      //   },
-      //   {
-      //     type: 'note',
-      //     id: 'skills'
-      //   },
-      //   {
-      //     type: 'note',
-      //     id: 'certifications'
-      //   },
-      //   {
-      //     type: 'note',
-      //     id: 'education'
-      //   },
-      //   {
-      //     type: 'note',
-      //     id: 'additional-experience'
-      //   }
-      // ]
-    }
-  ]
+  {
+    type: 'note',
+    id: 'about',
+    label: 'About'
+  },
+  {
+    type: 'category',
+    label: 'Career Journey',
+    link: { type: 'note', id: 'career-journey' },
+    items: [
+      {
+        type: 'note',
+        id: 'experience',
+        label: 'Experience'
+      },
+      {
+        type: 'note',
+        id: 'skills',
+        label: 'Skills'
+      },
+      {
+        type: 'note',
+        id: 'certifications',
+        label: 'Certifications'
+      },
+      {
+        type: 'note',
+        id: 'education',
+        label: 'Education'
+      },
+      {
+        type: 'note',
+        id: 'additional-experience',
+        label: 'Additional Experience'
+      }
+    ]
+  },
+  {
+    type: 'category',
+    label: 'Showcase',
+    link: { type: 'note', id: 'showcase' },
+    items: [
+      {
+        type: 'note',
+        id: 'a-data-engineer-journey',
+        label: "A Data Engineer's Journey"
+      }
+    ]
+  }
+]
